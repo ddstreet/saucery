457ae0bcf255f456fef9257b2f614858739c03c9
Dan Streetman <ddstreet@ieee.org>
2022-05-11 13:48:59 -0400

    rework saucery.js to better handle table sizing and fix conclusions display

--- web/saucery.js
+++ web/saucery.js
@@ -28,21 +28,15 @@ function SauceryMenuReady(callback) {
 $(() => $('.mainindex').css('text-align', 'center').append($('<a>').attr('href', '/').text('Return to main index')));
 
 // keys are fields, values are functions to generate hrefs
-var SauceryAnchorHrefFunctions = {
-    'case': ((v, e) => '/case/' + v),
-    'customer': ((v, e) => '/customer/' + v),
-    'hostname': ((v, e) => '/hostname/' + v),
-    'machineid': ((v, e) => '/machineid/' + v),
-    'name': ((v, e) => '/sos/' + v),
-    'sosreport': ((v, e) => '/sos/' + v),
-}
-var SauceryAnchorTextFunctions = {
-    'datetime': ((v, e) => DateToYMD(new Date(v))),
-    'conclusions_critical': ((v, e) => v.toString()),
-    'conclusions_error': ((v, e) => v.toString()),
-    'conclusions_warning': ((v, e) => v.toString()),
-    'conclusions_info': ((v, e) => v.toString()),
-    'conclusions_debug': ((v, e) => v.toString()),
+var SauceryAnchorFunctions = {
+    'case': ((v, e) => CreateAnchor(v, '/case/' + v)),
+    'customer': ((v, e) => CreateAnchor(v, '/customer/' + v, 30)),
+    'hostname': ((v, e) => CreateAnchor(v, '/hostname/' + v, 20)),
+    'machineid': ((v, e) => CreateAnchor(v, '/machineid/' + v, 12)),
+    'name': ((v, e) => CreateAnchor(v, '/sos/' + v, 30)),
+    'sosreport': ((v, e) => CreateAnchor(v, '/sos/' + v)),
+    'datetime': ((v, e) => CreateAnchor(DateToYMD(new Date(v)))),
+    'conclusions': ((v, e) => ConclusionsSummary(v)),
 }
 // entry fields that should be updated on page load
 var SauceryAnchorFields = [
@@ -62,32 +56,17 @@ var SauceryTableHeaders = {
     'machineid': 'Machine ID',
     'datetime': 'Date',
     'name': 'SOS',
-    'conclusions_critical': 'Criticals',
-    'conclusions_error': 'Errors',
-    'conclusions_warning': 'Warnings',
-    'conclusions_info': 'Infos',
-    'conclusions_debug': 'Debugs',
+    'conclusions': 'Conclusions',
 }
 // table column fields, in order they should be displayed
-var SauceryTableFields = [
-    'customer',
-    'case',
-    'hostname',
-    'machineid',
-    'datetime',
-    'name',
-    'conclusions_critical',
-    'conclusions_error',
-    'conclusions_warning',
-]
+var SauceryTableFields = Object.keys(SauceryTableHeaders);
 
 // params is an object with optional fields:
 //   filter: a function passed to entries.filter(); this overrides field/value filtering
 //   field: a field of each entry to filter on, default SauceryService
 //   value: the value to match each entry field with, default SauceryServiceValue
 //   anchorFields: Array of fields to update DOM elements, default SauceryAnchorFields
-//   anchorHrefFunctions: Object in same format as default, SauceryAnchorHrefFunctions
-//   anchorTextFunctions: Object in same format as default, SauceryAnchorTextFunctions
+//   anchorFunctions: Object in same format as default, SauceryAnchorFunctions
 //   tableFields: Array of fields to include in table, default SauceryTableFields
 //   tableHeaders: Object in same format as default, SauceryTableHeaders
 function SelectSOSEntries(params={}) {
@@ -97,8 +76,7 @@ function SelectSOSEntries(params={}) {
         filterfunction = (entry => encodeURIComponent(entry[params.field || SauceryService]) == (params.value || SauceryServiceValue));
 
     anchorFields = params.anchorFields || SauceryAnchorFields;
-    anchorHrefFunctions = params.anchorHrefFunctions || SauceryAnchorHrefFunctions;
-    anchorTextFunctions = params.anchorTextFunctions || SauceryAnchorTextFunctions;
+    anchorFunctions = params.anchorFunctions || SauceryAnchorFunctions;
     tableFields = params.tableFields || SauceryTableFields;
     tableHeaders = params.tableHeaders || SauceryTableHeaders;
 
@@ -115,40 +93,31 @@ function SelectSOSEntries(params={}) {
                 .sort((a, b) => a[field].toString().localeCompare(b[field].toString()));
 
             let anchors = entries.map(function (entry) {
-                return SauceryAnchor(entry, field, anchorTextFunctions[field], anchorHrefFunctions[field]);
+                return SauceryAnchor(entry, field, anchorFunctions[field]);
             });
 
             $('.' + field).append(JoinAnchors(anchors));
         });
 
-        SauceryTable('.SauceryTable', SOSEntries, tableFields, tableHeaders, anchorTextFunctions, anchorHrefFunctions);
+        SauceryTable('.SauceryTable', SOSEntries, tableFields, tableHeaders, anchorFunctions);
         SauceryTree('.SauceryTree');
     });
 }
 
-function SauceryAnchor(entry, field, textfunction, hreffunction) {
-    if (textfunction === undefined)
-        textfunction = SauceryAnchorTextFunctions[field];
-    if (hreffunction === undefined)
-        hreffunction = SauceryAnchorHrefFunctions[field];
+function SauceryAnchor(entry, field, anchorFunction) {
+    if (anchorFunction === undefined)
+        anchorFunction = SauceryAnchorFunctions[field];
 
-    let value = entry[field];
-    let text = textfunction ? textfunction(value, entry) : value;
-    let href = hreffunction ? hreffunction(encodeURIComponent(value), entry) : undefined;
-    let a = $('<a>').text(text);
-
-    if (href)
-        a.attr('href', href);
-
-    return a;
+    return (anchorFunction ?
+            anchorFunction(entry[field], entry) :
+            $('<a>').text(entry[field]));
 }
 
 function SauceryTable(elements,
                       entries,
                       fields=SauceryTableFields,
                       headers=SauceryTableHeaders,
-                      textfunctions=SauceryAnchorTextFunctions,
-                      hreffunctions=SauceryAnchorHrefFunctions) {
+                      anchorFunctions=SauceryAnchorFunctions) {
     let table = $('<table>');
     let thead = $('<thead>').appendTo(table);
     let tbody = $('<tbody>').appendTo(table);
@@ -162,7 +131,7 @@ function SauceryTable(elements,
     entries.forEach(function (entry) {
         let tr = $('<tr>').appendTo(tbody);
         fields.forEach(function (field) {
-            let anchor = SauceryAnchor(entry, field, textfunctions[field], hreffunctions[field]);
+            let anchor = SauceryAnchor(entry, field, anchorFunctions[field]);
             $('<td>').append(anchor).appendTo(tr);
         });
     });
@@ -212,14 +181,50 @@ function SauceryTree(elements) {
         .appendTo(elements);
 }
 
+function JoinAnchors(anchors, separatortext=', ') {
+    let list = anchors.flatMap(a => [a, $('<a>').text(separatortext)]);
+    list.pop();
+    return list;
+}
+
 function DateToYMD(date) {
     return (String(date.getUTCFullYear()).padStart(4, '0') + '-' +
             String(date.getUTCMonth()+1).padStart(2, '0') + '-' +
             String(date.getUTCDate()).padStart(2, '0'));
 }
 
-function JoinAnchors(anchors, separatortext=', ') {
-    let list = anchors.flatMap(a => [a, $('<a>').text(separatortext)]);
-    list.pop();
-    return list;
+function ConclusionsSummary(conclusions) {
+    let p = $('<p>');
+
+    if (conclusions.critical > 0)
+        p.append($('<a>').text('Critical: ' + conclusions.critical).addClass('critical')).append('<br>');
+    if (conclusions.error > 0)
+        p.append($('<a>').text('Error: ' + conclusions.error).addClass('error')).append('<br>');
+    if (conclusions.warning > 0)
+        p.append($('<a>').text('Warning: ' + conclusions.warning).addClass('warning')).append('<br>');
+    if (conclusions.info > 0)
+        p.append($('<a>').text('Info: ' + conclusions.info)).append('<br>');
+
+    return p;
+}
+
+function CreateAnchor(text, href, maxlen) {
+    let a = $('<a>').text(LimitLength(text, maxlen));
+
+    if (href)
+        a.attr('href', href)
+
+    return a;
+}
+
+function LimitLength(value, length) {
+    if (!value || !length)
+        return value;
+
+    let v = value.slice(0, length - 1);
+
+    if (v.length < value.length)
+        v = v + '\u2026';
+
+    return v;
 }
