c47bf30a1d5797c0823fa1d66b0b00170d0c2c70
Dan Streetman <ddstreet@ieee.org>
2022-05-24 14:16:47 -0400

    fix sosreport meta cache implementation

--- saucery/sos/meta.py
+++ saucery/sos/meta.py
@@ -18,13 +18,25 @@ class SOSMetaProperty(property):
     def __init__(self, name, valuetype=str, valuelist=False):
         # If valuelist=True, the content is treated as a whitespace-separated list
         self.name = name
-        self.valuetype = valuetype
+        self.cachename = f'_{name}_cache'
+        self._valuetype = valuetype
         self.valuelist = valuelist
-        self.cache = None
         super().__init__(self.read, self.write, self.unlink)
+
+    def get_cache(self, sos):
+        return getattr(sos, self.cachename)
+
+    def set_cache(self, sos, value):
+        setattr(sos, self.cachename, value)
+
+    def del_cache(self, sos):
+        delattr(sos, self.cachename)
 
     def path(self, sos):
         return sos.workdir / self.name
+
+    def valuetype(self, *args):
+        return self._valuetype(*args)
 
     def value(self, strvalue):
         if self.valuelist:
@@ -40,18 +52,21 @@ class SOSMetaProperty(property):
             value = ' '.join(map(self.strvalue, value))
         return str(value or '')
 
-    def read(self, sos):
-        if self.cache is not None:
-            return self.value(self.cache)
+    def _read(self, sos):
+        with suppress(AttributeError):
+            return self.get_cache(sos)
         with suppress(FileNotFoundError):
-            return self.value(self.path(sos).read_text().strip())
-        return self.value('')
+            return self.path(sos).read_text()
+        return ''
+
+    def read(self, sos):
+        return self.value(self._read(sos).strip())
 
     def write(self, sos, value):
         value = self.strvalue(value)
         if value and '\n' not in value:
             value += '\n'
-        self.cache = value
+        self.set_cache(sos, value)
         if not sos.dry_run:
             path = self.path(sos)
             if not path.exists():
@@ -59,29 +74,29 @@ class SOSMetaProperty(property):
             path.write_text(value)
 
     def unlink(self, sos):
-        self.cache = None
+        self.del_cache(sos)
         if not sos.dry_run:
             self.path(sos).unlink(missing_ok=True)
 
 
 class BoolSOSMetaProperty(SOSMetaProperty):
-    def __init__(self, name, valuetype=bool, **kwargs):
-        super().__init__(name, valuetype=lambda v: v == 'True', **kwargs)
+    def valuetype(self, *args):
+        return args and str(args[0]).strip().lower() == 'true'
 
     def strvalue(self, value):
         return str(value is True)
 
 
 class JSONSOSMetaProperty(SOSMetaProperty):
-    def strvalue(self, value):
-        if value is None:
-            return ''
-        return json.dumps(value, indent=2)
-
     def value(self, strvalue):
         if not strvalue:
             return ''
         return json.loads(strvalue)
+
+    def strvalue(self, value):
+        if value is None:
+            return ''
+        return json.dumps(value, indent=2)
 
 
 class SOSMetaDict(MutableMapping):
