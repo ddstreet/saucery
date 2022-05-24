dc80b4c161240fee1ab26b32ade9a0d888cfe8e1
Dan Streetman <ddstreet@ieee.org>
2022-05-24 14:15:20 -0400

    add test data and initial unit test

---
+++ tests/test_saucery.py
@@ -0,0 +1,44 @@
+
+import shutil
+import tempfile
+import unittest
+
+from pathlib import Path
+from saucery import Saucery
+
+
+TEST_DATA_PATH = Path(__file__).parent / 'saucery_data'
+TEST_SOS = 'sosreport-sosreport-f-123456-2022-05-23-qcpndam.tar.xz'
+
+
+class SauceryTest(unittest.TestCase):
+    def setUp(self):
+        self.testdir = tempfile.TemporaryDirectory()
+        saucery = shutil.copytree(TEST_DATA_PATH, Path(self.testdir.name) / 'saucery')
+        self.saucery = Saucery(saucery=saucery)
+
+    def tearDown(self):
+        self.testdir.cleanup()
+
+    def testSauceryContent(self):
+        self.assertGreater(len(self.saucery.sosreports), 0)
+        sos = self.saucery.sosreports[0]
+        self.assertFalse(sos.extracted)
+        self.assertFalse(sos.invalid)
+        self.assertFalse(sos.seared)
+
+    def testExtractSear(self):
+        sos = self.saucery.sosreport(TEST_SOS)
+        self.assertTrue(sos.exists())
+        self.assertFalse(sos.extracted)
+
+        sos.extract()
+        self.assertTrue(sos.extracted)
+        self.assertFalse(sos.invalid)
+        self.assertFalse(sos.seared)
+        self.assertGreater(sos.file_count, 0)
+        self.assertIsNotNone(sos.file_list)
+        self.assertEqual(sos.hostname, 'sosreport-f')
+
+        sos.sear()
+        self.assertEqual(sos.case, '123456')
