e552ca858e9be250f8034411a30b1dd93e110c8b
Dan Streetman <ddstreet@canonical.com>
2022-06-27 14:36:00 -0400

    reference: remove bytesreference and dictreference

--- saucery/reduction/reference/__init__.py
+++ saucery/reduction/reference/__init__.py
@@ -1,9 +1,7 @@
 
 # We have to import all the implementation files so they
 # 'register' their type with Definition
-from .bytesreference import * # noqa
 from .chain import * # noqa
-from .dictreference import * # noqa
 from .file import * # noqa
 from .inireference import * # noqa
 from .jqreference import * # noqa
