a6df5f94ddc35da937aea045a595fd3489826a27
Dan Streetman <ddstreet@ieee.org>
2022-06-30 12:26:04 -0400

    reference: combine exec and jqreference into parse module

--- saucery/reduction/reference/__init__.py
+++ saucery/reduction/reference/__init__.py
@@ -2,10 +2,8 @@
 # We have to import all the implementation files so they
 # 'register' their type with Definition
 from .chain import * # noqa
-from .exec import * # noqa
 from .file import * # noqa
 from .inireference import * # noqa
-from .jqreference import * # noqa
 from .parse import * # noqa
 
 from .reference import InvalidReferenceError
--- saucery/reduction/reference/parse.py
+++ saucery/reduction/reference/parse.py
@@ -1,6 +1,10 @@
 
+import subprocess
+
 from abc import abstractmethod
+from collections import ChainMap
 from functools import cached_property
+from shutil import which
 
 from .path import ReferencePathList
 from .reference import Reference
@@ -32,18 +36,23 @@ class ParseReference(Reference):
 
     @property
     def parse_none_value(self):
+        '''If a None parameter should be provided to parse().
+
+        If False (the default), the parse() method will not be called if the pathlist provided
+        to it is None, or if the pathlist.value is None. If True, parse() will be called even
+        if the pathlist or pathlist.value is None.
+        '''
         return False
 
     @cached_property
     def pathlist(self):
         source = self.reductions.reference(self.source)
-        if source is None or source.pathlist is None:
-            return None
+        pathlist = getattr(source, 'pathlist', None)
+        if not self.parse_none_value:
+            if pathlist is None or pathlist.value is None:
+                return None
 
-        if not self.parse_none_value and source.pathlist.value is None:
-            return None
-
-        return self.parse(source.pathlist)
+        return self.parse(pathlist)
 
 
 class TransformReference(ParseReference):
@@ -67,6 +76,87 @@ class TransformReference(ParseReference):
         pass
 
     def parse(self, pathlist):
-        self.sos.analysis_files[self.name] = self.transform(pathlist.value)
+        value = getattr(pathlist, 'value', None)
+        if not self.parse_none_value and value is None:
+            return None
+
+        self.sos.analysis_files[self.name] = self.transform(value)
 
         return ReferencePathList([self.sos.analysis_files.path(self.name)])
+
+
+class ExecReference(TransformReference):
+    '''ExecReference object.
+
+    This extends TransformReference, and runs an external program to transform the source value.
+
+    The 'exec' field must be the name of the program to run, and 'params' should (optionally)
+    be either a single str param value or list of str param values. Any {}-style format fields
+    in any of the params will be replaced with the value of field from the sos, for example
+    {filesdir} will be replaced with the actual full path of the sos.filesdir.
+
+    Unlike most References, the 'source' field is optional, in which case no stdin will be
+    provided to the external program. If the 'source' field is provided, the external
+    program will only be called if the source reference provides a value.
+    '''
+    @classmethod
+    def TYPE(cls):
+        return 'exec'
+
+    @classmethod
+    def fields(cls):
+        return ChainMap({'source': cls._field('text', default=''),
+                         'exec': cls._field('text'),
+                         'params': cls._field(['text', 'list'], default=[])},
+                        super().fields())
+
+    def setup(self):
+        super().setup()
+        # Find our exec binary
+        self.exec_cmd
+
+    @property
+    def parse_none_value(self):
+        # Allow exec without any source
+        # e.g. to use external program to process the entire sos filesdir/
+        return not self.source
+
+    @cached_property
+    def exec_cmd(self):
+        cmd = which(self.get('exec'))
+        if not cmd:
+            self._raise("Could not find '{self.get('exec')}' command")
+        return [cmd] + self.sos.mapping.format(self.get('params'))
+
+    def transform(self, value):
+        result = subprocess.run(self.exec_cmd, input=value,
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE)
+        if result.returncode != 0:
+            return None
+        return result.stdout
+
+
+class JqReference(ExecReference):
+    '''JqReference object.
+
+    This extends ExecReference, and sets the default of the 'exec' field to 'jq'.
+
+    This requires the jq parsing str to be provided in the 'jq' field.
+
+    Unlike ExecReference, this does require a value source to parse.
+    '''
+    @classmethod
+    def TYPE(cls):
+        return 'jq'
+
+    @classmethod
+    def fields(cls):
+        return ChainMap({'source': cls._field('text'),
+                         'exec': cls._field('text', default='jq'),
+                         'jq': cls._field('text')},
+                        super().fields())
+
+    @property
+    def exec_cmd(self):
+        return super().exec_cmd + [self.get('jq')]
