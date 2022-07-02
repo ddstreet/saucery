196b2952c59733c26b3e75f2cd9921d2a11b208d
Dan Streetman <ddstreet@ieee.org>
2022-07-01 20:16:44 -0400

    reference/regex: change 'regex' field name to 'pattern', add 'multiline' and 'ignorecase' fields

--- saucery/reduction/reference/regex.py
+++ saucery/reduction/reference/regex.py
@@ -1,4 +1,6 @@
 
+import re
+
 from collections import ChainMap
 
 from .parse import ParseReference
@@ -7,8 +9,12 @@ from .parse import ParseReference
 class RegexReference(ParseReference):
     '''RegexReference class.
 
-    The 'regex' field must be a python regex pattern, which will be matched to our source
+    The 'pattern' field must be a python regex pattern, which will be matched to our source
     value. This will create a new pathlist containing only matched section(s).
+
+    The 'multiline' field sets the regex MULTILINE mode. It is True by default.
+
+    The 'ignorecase' field sets the regex IGNORECASE mode. It is False by default.
     '''
     @classmethod
     def TYPE(cls):
@@ -16,8 +22,23 @@ class RegexReference(ParseReference):
 
     @classmethod
     def fields(cls):
-        return ChainMap({'regex': cls._field('text')},
+        return ChainMap({'pattern': cls._field('text'),
+                         'multiline': cls._field('bool', default=True),
+                         'ignorecase': cls._field('bool', default=False)},
                         super().fields())
 
+    @property
+    def _pattern(self):
+        return self.get('pattern')
+
+    @property
+    def _flags(self):
+        flags = 0
+        if self.get('multiline'):
+            flags |= re.MULTILINE
+        if self.get('ignorecase'):
+            flags |= re.IGNORECASE
+        return flags
+
     def parse(self, pathlist):
-        return pathlist.regex_pathlist(self.get('regex'))
+        return pathlist.regex_pathlist(self._pattern, self._flags)
